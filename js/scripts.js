a67bd2c687726f0d005d0d9a7df5a92a7a281d85
Hugo-Ferrari <hugoferrari36@gmail.com>
2025-05-15 20:35:50 -0300

    exercicios 1 e 2

--- js/scripts.js
+++ js/scripts.js
@@ -11,6 +11,11 @@ function exe0(){
     // mostra o resultado
      alert(`A média das notas é ${media.toFixed(1)}`)
 }
+
+
+
+
+
 function pesquisaSatisfacao(){
     let nota // guarda as notas do participante
     let conta = 1 // vai contar de 1 a 10
@@ -36,6 +41,78 @@ function pesquisaSatisfacao(){
     let media = soma / 10
     alert(`A média é ${media.toFixed(1)}`)
 }
+
+
+function exe1(){
+    let a,b,c,d, i, aux    // aux = auxiliar 
+    let grupo =1
+    while(grupo<=5){
+    a= Number(prompt("informe o valor de a "))
+    b= Number(prompt("informe o valor de b "))
+    c= Number(prompt("informe o valor de c "))
+    d= Number(prompt("informe o valor de d "))
+    i=0
+
+    while(i<=3){
+        
+        if(a>b){
+            aux =a; a=b; b=aux
+        }
+
+        if(b>c){
+            aux = b; b=c; c=aux
+        }
+        if(c>d){
+            aux=c; c=d; d=aux
+        }
+        
+        i++
+    
+
+    }
+    alert (`ordem crescente: ${a} ${b} ${c} ${d} `)
+    alert (`ordem decrescente: ${d} ${c} ${b} ${a} `)
+
+    grupo++
+    }
+}
+function exe2(){
+    let preco = 5.0
+    let quant = 120 
+    let lucro 
+    let despesa =200
+    let aux =""
+    while(preco>= 1.0){
+        lucro = (preco* quant)- despesa
+        aux= aux + "\n"+ ( $(preco) - $(quant) - $(despesa) - $(lucro))
+        quant =quant +25
+        preco = preco - 0.5
+    }
+    alert( aux)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function exe3(){
     let conta, idade, soma, porcentagem 
